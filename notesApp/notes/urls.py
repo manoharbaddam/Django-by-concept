a41a2b8a0269cc5b117a3a2e102dffb0c6357ded
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2025-12-24 12:22:49 +0530

    update: CRUD operations

--- notesApp/notes/urls.py
+++ notesApp/notes/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('create/',views.create_note,name="create_note"),
-
+    path('<int:note_id>/view',views.view_note,name="view"),
+    path('<int:note_id>/edit',views.edit_note,name="edit_note"),
+    path('<int:note_id>/delete',views.delete_note,name="delete_note"),
 ]
 
 
--- notesApp/notes/views.py
+++ notesApp/notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Notes
 from .forms import CreateForm
@@ -20,4 +20,25 @@ def create_note(request):
 
     return render(request,'notes/create_note.html',{'form':form})
 
+def view_note(request,note_id):
+    note = Notes.objects.get(id=note_id)
+    return render(request,'notes/view_note.html',{'note':note})
 
+def edit_note(request,note_id):
+    note = get_object_or_404(Notes,id=note_id)
+    if request.method=="POST":
+        form = CreateForm(request.POST,instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = CreateForm(instance=note)
+    return render(request,'notes/edit_note.html',{'form':form})
+
+def delete_note(request,note_id):
+    note = get_object_or_404(Notes,id=note_id)
+    if request.method=="POST":
+        note.delete()
+        return redirect("index")
+
+    return render(request,"notes/delete_note.html")
