5e82c05d12b78bc33dd03bbbcb316d582b42d487
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2025-12-28 14:48:39 +0530

    update: UserSpecific journals

--- 04_MyJournalUserSpecific/personalJournal/urls.py
+++ 04_MyJournalUserSpecific/personalJournal/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('',views.index,name="index"),  
+    path('myjournal/',views.user_journal,name="user_journal"),
     path('create/',views.create_journal,name="create_journal"),
     path('<int:journal_id>/view/',views.view_journal,name='view_journal'),
     path('<int:journal_id>/edit/',views.edit_journal,name="edit_journal"),
--- 04_MyJournalUserSpecific/personalJournal/views.py
+++ 04_MyJournalUserSpecific/personalJournal/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from  django.contrib.auth.decorators import login_required
+
 from .forms import JournalCreationForm,UserRegistrationForm
 from .models import Journal
 
@@ -10,6 +12,12 @@ def index(request):
     journals = Journal.objects.all()
     return render(request,'index.html',{'journals':journals})
 
+@login_required
+def user_journal(request):
+    journals = Journal.objects.filter(author=request.user)
+    return render(request,'user_journal.html',{'journals':journals})
+
+@login_required
 def create_journal(request):
     if request.method=="POST":
         form = JournalCreationForm(request.POST,request.FILES)
@@ -17,33 +25,34 @@ def create_journal(request):
             journal = form.save(commit=False)
             journal.author = request.user
             journal.save()
-            return redirect('index')
+            return redirect('user_journal')
     else:
         form  = JournalCreationForm()
     return render(request,'create_journal.html',{'form':form})
 
-
+@login_required
 def view_journal(request,journal_id):
     journal = get_object_or_404(Journal,id=journal_id)
     return render(request,'view_journal.html',{'journal':journal})
 
+@login_required
 def edit_journal(request,journal_id):
     journal  = get_object_or_404(Journal,id=journal_id)
     if request.method=="POST":
         form = JournalCreationForm(request.POST,request.FILES,instance=journal)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('user_journal')
     else:
         form = JournalCreationForm(instance=journal)
     return render(request,'edit_journal.html',{'form':form})
 
-
+@login_required
 def delete_journal(request,journal_id):
     journal = get_object_or_404(Journal,id=journal_id)
     if request.method=="POST":
         journal.delete()    
-        return redirect('index')
+        return redirect('user_journal')
     return render(request,'delete_journal.html',{'journal':journal})
 
 def register(request):
@@ -51,7 +60,7 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('login_view')
     else:
         form = UserRegistrationForm()
     return render(request,'registration/register.html',{'form':form})
@@ -64,7 +73,7 @@ def login_view(request):
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
-            return redirect('index')
+            return redirect('user_journal')
     return render(request,'registration/login.html')
 
 
