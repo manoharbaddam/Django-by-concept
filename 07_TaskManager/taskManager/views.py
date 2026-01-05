7df0ea90ab4a0849b0b95f731eafa5ae5378df9d
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2026-01-05 21:09:11 +0530

    Update index page

--- 07_TaskManager/taskManager/views.py
+++ 07_TaskManager/taskManager/views.py
@@ -7,7 +7,7 @@ from .models import Project,Task
 # Create your views here.
 
 def index(request):
-    return render(request,'layout.html')
+    return render(request,'index.html')
 
 def create_project(request):
     form = ProjectCreationForm()
