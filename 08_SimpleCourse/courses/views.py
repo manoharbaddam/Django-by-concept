b43acc6dda6234f7605f47ebc630fcaff42b2863
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2026-01-11 14:27:23 +0530

    Add view for course creation

--- 08_SimpleCourse/courses/views.py
+++ 08_SimpleCourse/courses/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+def create_Course(request):
+    if request.method=="POST":
+        form = CourseCreationForm(request.POST)
+        form.save()
+        return redirect('index')
+
+    return render(request,'index')
