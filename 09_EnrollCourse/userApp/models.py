3cf18bfd14619f6b8fb7d1b9cf9f1a1d4f241164
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2026-01-16 20:45:19 +0530

    Create Custom User Model

--- 09_EnrollCourse/userApp/models.py
+++ 09_EnrollCourse/userApp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password,**other_fields):
+        if not email:
+            raise ValueError("Enter valid Email Id.")
+        
+        email = self.normalize_email(email)
+
+        user = self.create(email=email,**other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self,email,password,**other_fields):
+        other_fields.setdefault('is_superuser',True)
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_active',True)
+
+        if other_fields.get('is_superuser')==False:
+            raise ValueError("Superuser should have is_staff set to True.")
+        if other_fields.get('is_staff')==False:
+            raise ValueError("Superuser should have is_staff set to True.")
+        
+        self.create_user(email=email,password=password,**other_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default = False)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name']
