8c20c15c28819b788c6cd732b003748e4f824280
Baddam Manohar Reddy <manoharbaddam789@gmail.com>
2026-02-05 20:53:22 +0530

    Add view to display assigned classes

--- 10_School_System/courses/urls.py
+++ 10_School_System/courses/urls.py
@@ -6,6 +6,7 @@ app_name="courses"
 urlpatterns = [
     path("create/", views.create_class, name="create_class"),
     path("classes/", views.all_classes, name="all_classes"),
-    path('<str:username>/classes',views.student_classes,name="student_classes"),
+    path('student=<str:username>/classes',views.student_classes,name="student_classes"),
+    path('teacher=<str:username>/classes',views.teacher_classes,name="teacher_classes"),
     path('enroll/<int:class_id>/', views.enroll_class, name='enroll_class'),
 ]
--- 10_School_System/courses/views.py
+++ 10_School_System/courses/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .forms import ClassCreateForm
-from .models import Class,Enrollment
+from .models import Class, Enrollment
 from users.models import CustomUser
+
 
 @login_required
 def create_class(request):
@@ -14,31 +15,45 @@ def create_class(request):
         form = ClassCreateForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("courses:class_list")
+            return redirect("courses:create_class")
     else:
         form = ClassCreateForm()
 
     return render(request, "create_class.html", {"form": form})
 
+
 @login_required
 def all_classes(request):
     classes = Class.objects.select_related("teacher")
 
-    return render(request,"class_list.html",{"classes": classes})
+    return render(request, "class_list.html", {"classes": classes})
 
-@login_required()
-def student_classes(request,username=None):
-    if request.user.role == "TEACHER":
+
+@login_required
+def student_classes(request, username=None):
+    print(request.user.role)
+    if request.user.role != "STUDENT":
         return HttpResponseForbidden("Access denied")
-    
+
     classes_enrolled = Enrollment.objects.filter(student=request.user)
-    return render(request,'student_enrollments.html',{"classes":classes_enrolled})
+    return render(request, "student_enrollments.html", {"classes": classes_enrolled})
+
+
+@login_required
+def teacher_classes(request,username=None):
+    if request.user.role != "TEACHER":
+        print(request.user.role)
+        return HttpResponseForbidden("Access Denied")
+
+    classes_assigned = Class.objects.filter(teacher=request.user)
+    return render(
+        request, "teacher_classes.html", {"classes_assigned": classes_assigned}
+    )
+
 
 def enroll_class(request, class_id):
     if request.method == "POST":
         cls = get_object_or_404(Class, id=class_id)
-        # Assuming you have an Enrollment model or many-to-many relationship
-        # cls.student.add(request.user)
         Enrollment.objects.create(student=request.user, class_enrolled=cls)
-        return redirect('courses:student_classes', username=request.user.username)
-    return redirect('courses:all_classes') 
+        return redirect("courses:student_classes", username=request.user.username)
+    return redirect("courses:all_classes")
